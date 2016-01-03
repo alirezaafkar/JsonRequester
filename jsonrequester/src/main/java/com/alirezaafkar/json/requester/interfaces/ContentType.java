86ed2abdc2578bfbd7c1ebc0c2dcd5664d3b0db3
Alireza Afkar <pesiran@gmail.com>
2016-01-03 12:53:53 +0330

    Updated version

--- jsonrequester/src/main/java/com/alirezaafkar/json/requester/interfaces/ContentType.java
+++ jsonrequester/src/main/java/com/alirezaafkar/json/requester/interfaces/ContentType.java
@@ -2,11 +2,15 @@ package com.alirezaafkar.json.requester.interfaces;
 
 import android.support.annotation.StringDef;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by Alireza Afkar on 12/11/15 AD.
  */
 public class ContentType {
     @StringDef({TYPE_JSON, TYPE_FORM})
+    @Retention(RetentionPolicy.SOURCE)
     public @interface Type {
     }
 
